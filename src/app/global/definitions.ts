1e2aa17f4af73a7190842e5d8e74084e1b468c43
Pietro Gabba <gabba.pietro@gmail.com>
2020-02-26 10:32:51 +0100

    first commit?

---
+++ src/app/global/definitions.ts
@@ -0,0 +1,44 @@
+// circolo delle quinte (alterazioni #)
+var circleOfFifth = [
+    { note: "C", alterations: 0},
+    { note: "G", alterations: 1},
+    { note: "D", alterations: 2},
+    { note: "A", alterations: 3},
+    { note: "E", alterations: 4},
+    { note: "B", alterations: 5},
+    { note: "F#", alterations: 6},
+    { note: "C#", alterations: 7}
+];
+
+var circleOfFifthAlterations = [
+    {index: 0, alteration: "F"},
+    {index: 1, alteration: "C"},
+    {index: 2, alteration: "G"},
+    {index: 3, alteration: "D"},
+    {index: 4, alteration: "A"},
+    {index: 5, alteration: "E"},
+    {index: 6, alteration: "B"},
+];
+
+// circolo delle quarte (alterazioni b)
+var circleOfFourth = [
+    { note: "C", alterations: 0},
+    { note: "F", alterations: 1},
+    { note: "Bb", alterations: 2},
+    { note: "Eb", alterations: 3},
+    { note: "Ab", alterations: 4},
+    { note: "Db", alterations: 5},
+    { note: "Gb", alterations: 6},
+    { note: "Cb", alterations: 7}
+];
+
+var circleOfFourthAlterations = [
+    {index: 0, alteration: "B"},
+    {index: 1, alteration: "E"},
+    {index: 2, alteration: "A"},
+    {index: 3, alteration: "D"},
+    {index: 4, alteration: "G"},
+    {index: 5, alteration: "C"},
+    {index: 6, alteration: "F"},
+];
+
