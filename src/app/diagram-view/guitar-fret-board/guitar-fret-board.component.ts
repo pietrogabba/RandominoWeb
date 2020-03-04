571bab86ca6096859000f9071e5070995f38c2da
Pietro Gabba <gabba.pietro@gmail.com>
2020-03-04 14:34:24 +0100

    Trovato modo per iterare sui fret

--- src/app/diagram-view/guitar-fret-board/guitar-fret-board.component.ts
+++ src/app/diagram-view/guitar-fret-board/guitar-fret-board.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+import { GuitarFretComponent } from '../guitar-fret/guitar-fret.component';
 
 @Component({
   selector: 'app-guitar-fret-board',
   templateUrl: './guitar-fret-board.component.html',
   styleUrls: ['./guitar-fret-board.component.scss']
 })
-export class GuitarFretBoardComponent implements OnInit {
+export class GuitarFretBoardComponent implements OnInit, AfterViewInit {
+  //TODO: eh eh! Questo ti permette di iterare sui fret (dio cane)
+  @ViewChildren('apfret') frets: QueryList<GuitarFretComponent>;
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+  ngAfterViewInit(): void {
 
+    this.frets.forEach(fret => console.log('fret: ' + fret.fretNote))
+
+  }
 }
