892de39de0c30bafff1cd6610a6056ffcd93883c
Pietro Gabba <gabba.pietro@gmail.com>
2020-03-05 15:29:34 +0100

    Prima versione abbozzata della keyboard view

--- src/app/diagram-view/key-board/key-board.component.ts
+++ src/app/diagram-view/key-board/key-board.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChildren, QueryList, OnChanges } from '@angular/core';
+import { KeyComponent } from '../key/key.component';
 
 @Component({
   selector: 'app-key-board',
   templateUrl: './key-board.component.html',
   styleUrls: ['./key-board.component.scss']
 })
-export class KeyBoardComponent implements OnInit {
+export class KeyBoardComponent implements OnInit, OnChanges {
+  @Input() calculatedScaleArray: Array<string>;
+  @Input() tonicNote: string;
+  @ViewChildren('apkey') keys: QueryList<KeyComponent>;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    if(this.calculatedScaleArray){
+      this.keys.forEach(key => {
+
+        if(this.calculatedScaleArray.includes(key.leftNote)){
+          let isTonic = (key.leftNote === this.tonicNote);
+          key.setActiveLeftNote(isTonic);
+        }else
+          key.setInactiveLeftNote();
+
+        if(this.calculatedScaleArray.includes(key.note)){
+          let isTonic = (key.note === this.tonicNote);
+          key.setActiveCenterNote(isTonic);
+        }else
+          key.setInactiveCenterNote();
+
+        if(this.calculatedScaleArray.includes(key.rightNote)){
+          let isTonic = (key.rightNote === this.tonicNote);
+          key.setActiveRightNote(isTonic);
+        }else
+          key.setInactiveRightNote();
+      });
+    };
   }
 
 }
--- src/app/diagram-view/key/key.component.ts
+++ src/app/diagram-view/key/key.component.ts
@@ -6,13 +6,49 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./key.component.scss']
 })
 export class KeyComponent implements OnInit {
+
   @Input() leftNote: string;
   @Input() note: string;
   @Input() rightNote: string;
 
-  constructor() { }
+  @Input() activeLeft: boolean = false;
+  @Input() activeCenter: boolean = false;
+  @Input() activeRight: boolean = false;
+
+  activeIcon: string;
+
+  constructor() {
+    if(!this.activeIcon)
+    this.activeIcon = '';
+   }
 
   ngOnInit(): void {
+  }
+
+  setActiveLeftNote(isTonic: boolean) {
+    this.activeLeft = true;
+  }
+
+  setActiveRightNote(isTonic: boolean) {
+    this.activeRight = true;
+  }
+
+  setActiveCenterNote(isTonic: boolean) {
+    this.activeCenter = true;
+    this.activeIcon = 'play_circle_filled';
+  }
+
+  setInactiveLeftNote() {
+    this.activeLeft = false;
+  }
+
+  setInactiveRightNote() {
+    this.activeRight = false;
+  }
+
+  setInactiveCenterNote() {
+    this.activeCenter = false;    
+    this.activeIcon = '';
   }
 
 }
